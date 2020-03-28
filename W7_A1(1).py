dabc8494804eb03b78a48bc787f72f741a963eb7
Ramana <46435003+gonnabathula@users.noreply.github.com>
2020-03-27 21:41:52 -0400

    Add files via upload

---
+++ W7_A1(1).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "C:\\Users\\gchin\\Desktop\\DV\\Datasets\\all_040_in_12.P1.csv"
+df = pd.read_csv(Location)
+
+
+# In[2]:
+
+
+df.head()
+
